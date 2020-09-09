37afc31ccd3868acd02befab191ecad19965bcab
christopherklint <christopher.klint@gmail.com>
2020-09-09 18:14:26 +0200

    changing colors and getting rid of flex for the whole page

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
 'use strict';
 
-let i = 0;
-const txt = 'Hello there! My name is Chris and I am a Fullstack Developer. I specialize in React, Nodejs, and Python.';
-const speed = 75;
-
-typewriter();
-
-function typewriter() {
-    if (i < txt.length) {
-        document.querySelector('.hero-text').textContent += txt.charAt(i);
-        i++;
-        setTimeout(typewriter, speed);
-    }
-}
